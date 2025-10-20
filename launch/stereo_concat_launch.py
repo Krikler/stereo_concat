81587452c0dc41ba2fba31026a974b7dd5553b1e
Krikler <krikler.zalan@gmail.com>
2025-10-20 21:17:31 +0200

    Start ros bag in launch file

--- launch/stereo_concat_launch.py
+++ launch/stereo_concat_launch.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
 
@@ -11,6 +11,8 @@ def generate_launch_description():
 
     package_path = get_package_share_directory('stereo_concat')
     config = os.path.join(package_path, 'config', 'default_params.yaml')
+    
+    bag_path = os.path.expanduser('~/ros2_bags/onboarding.mcap')
 
     stereo_concat_node = Node(
         package='stereo_concat',
@@ -19,7 +21,13 @@ def generate_launch_description():
         output='screen',
     )
 
+    ros_bag_play = ExecuteProcess(
+        cmd=['ros2', 'bag', 'play', '-l', bag_path],
+        output='screen'
+    )
+
 
     return launch.LaunchDescription([
-        stereo_concat_node
+        stereo_concat_node,
+        ros_bag_play
     ])
