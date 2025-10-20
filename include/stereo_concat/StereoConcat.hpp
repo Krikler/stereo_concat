a27dd2738b7421875b83e6e7f455f075053e279a
Krikler <krikler.zalan@gmail.com>
2025-10-20 20:49:47 +0200

    Comment header files

--- include/stereo_concat/StereoConcat.hpp
+++ include/stereo_concat/StereoConcat.hpp
@@ -7,8 +7,16 @@ namespace stereo_concat {
 
 class StereoConcat {
 
+    // Store the latest concatenated image
     sensor_msgs::msg::Image latest_concatenated_image_ = sensor_msgs::msg::Image();
 
+    /**
+     * @brief Concatenates two compressed images side by side
+     * 
+     * @param leftImage The left compressed image.
+     * @param rightImage The right compressed image.
+     * @return The concatenated image.
+     */
     sensor_msgs::msg::Image concatImages(sensor_msgs::msg::CompressedImage leftImage, sensor_msgs::msg::CompressedImage rightImage);
 
     public:
@@ -17,8 +25,19 @@ class StereoConcat {
 
         ~StereoConcat() = default;
 
+        /**
+         * @brief Concatenates two images and store the result
+         * 
+         * @param leftImage The left compressed image.
+         * @param rightImage The right compressed image.
+         */
         void processImages(sensor_msgs::msg::CompressedImage leftImage, sensor_msgs::msg::CompressedImage rightImage);
 
+        /**
+         * @brief Get the latest concatenated image
+         * 
+         * @return The latest concatenated image.
+         */
         sensor_msgs::msg::Image getLatestConcatenatedImage();
 };
 
--- include/stereo_concat/StereoConcatNodeHandler.hpp
+++ include/stereo_concat/StereoConcatNodeHandler.hpp
@@ -14,18 +14,23 @@ namespace stereo_concat {
 
 class StereoConcatNodeHandler : public rclcpp::Node {
 
+    // The object that performs the concatenation
     StereoConcat stereo_concatenator_;
 
+    // Input image subscribers
     message_filters::Subscriber<sensor_msgs::msg::CompressedImage> left_image_subscriber_;
-
     message_filters::Subscriber<sensor_msgs::msg::CompressedImage> right_image_subscriber_;
 
+    // Output concatenated image publisher
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr concatenated_image_publisher_;
 
+    // Synchronizer for the input image subscribers
     std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::CompressedImage,sensor_msgs::msg::CompressedImage>> sync_;
 
+    // Timer for publishing
     rclcpp::TimerBase::SharedPtr timer_;
 
+    // Callback for synchronized input images
     void imagesCallback(const sensor_msgs::msg::CompressedImage::ConstSharedPtr& left_msg, const sensor_msgs::msg::CompressedImage::ConstSharedPtr& right_msg);
 
     public:
@@ -33,8 +38,14 @@ class StereoConcatNodeHandler : public rclcpp::Node {
 
         ~StereoConcatNodeHandler() = default;
 
+        /**
+         * @brief Initialize the node handler with subscribers, publishers
+         */
         void init();
 
+        /**
+         * @brief Timer callback to publish the latest concatenated image
+         */
         void onTimer();
 };
 
